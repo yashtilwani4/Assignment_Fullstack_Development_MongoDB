87d1a3b31b79b761471fa9b5953b812b7b56e19f
yashtilwani4 <116968481+yashtilwani4@users.noreply.github.com>
2025-02-18 02:29:13 +0530

    Update Q2.js

---
+++ Week 2 Assignment VIT_Fullstack_Development_MongoDB/Q2.js
@@ -0,0 +1,10 @@
+let x = 10;
+let y = 5;
+
+let addition = x + y;
+let multiplication = x * y;
+let modulus = x % y;
+
+console.log("Addition:", addition);        
+console.log("Multiplication:", multiplication); 
+console.log("Modulus:", modulus);         
